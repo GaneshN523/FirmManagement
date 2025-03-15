770e3067050d6b409936f8810a1ecdbc3f264241
GaneshN523 <ganeshnalawade523@gmail.com>
2025-03-15 22:58:13 +0530

    FirmManagement

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo/', // Replace with your GitHub repo name
-  server: {
-    historyApiFallback: true, // Ensures all routes are served from index.html
-}});
+  base: '/FirmManagement/', // Replace with your GitHub repo name
+});
